94d2a2d8bf47641d7f30be32d47b69d345ccaaa7
liuqiang <liuqiang@vowei.com>
2024-02-19 17:13:22 +0800

    基于tallrec框架和mind数据集实现大模型微调。

---
+++ src/finetune-llm/infer_tallrec.sh
@@ -0,0 +1,3 @@
+python infer_tallrec.py \
+    --base_model '/Users/liuqiang/Desktop/code/llm/models/chinese-alpaca-2-7b' \
+    --lora_weights '/Users/liuqiang/Desktop/code/llm4rec/llm4rec_abc/src/finetune-llm/lora-weights'
